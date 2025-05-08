ddc18bb6fad5c1c53904b7cf3117ee094aba3dbd
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-08 10:56:09 +0900

    Add default export,named export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import utils from './utils';
+import { helper1, helper2 } from './utils';
 
 function App() {
+	alert(utils());
+	alert(helper1());
+	alert(helper2());
   return (
 			<div className="App">
 			 Hello, React!
