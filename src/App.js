822a644c467a786d7b59ceb2789b3287eef7914a
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-08 11:35:33 +0900

    Add jsx examples

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import utils from './utils';
 import { helper1, helper2 } from './utils';
+import Button from './Button';
+import Counter from './Counter';
 
 function App() {
-	alert(utils());
-	alert(helper1());
-	alert(helper2());
+//	alert(utils());
+//	alert(helper1());
+//	alert(helper2());
   return (
 			<div className="App">
-			 Hello, React!
+			 Hello, React!<br/>
+			 <Button/><br/>
+			 <Counter/>
 			</div>
   );
 }
