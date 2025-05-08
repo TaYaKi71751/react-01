c9ee99127d6fa9aa285cb14833494124927c0e14
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-08 11:45:49 +0900

    Add examples

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import { helper1, helper2 } from './utils';
 import Button from './Button';
 import Counter from './Counter';
 
+function Greeting() {
+	return (
+		<h1>Hello, World!</h1>
+	);
+}
+
 function App() {
 //	alert(utils());
 //	alert(helper1());
 //	alert(helper2());
+	 const name = 'John';
+	 const element = <h2>Hello, {name}!</h2>;
   return (
 			<div className="App">
 			 Hello, React!<br/>
 			 <Button/><br/>
-			 <Counter/>
+			 <Counter/><br/>
+			 {element}{element}<br/>
+			 <Greeting/><Greeting/>
 			</div>
   );
 }
